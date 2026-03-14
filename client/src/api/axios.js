ec29be8ea2a499557f8d618618d00dae4134e479
Ambika Raj <ambikaraj2114@gmail.com>
2026-03-14 20:27:19 +0530

    just changed axios and deleted extra file before deployement

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,26 @@
+// import axios from 'axios'
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000',
+// })
+
+// // Automatically attach token to every request
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default API
+
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
 })
 
-// Automatically attach token to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
