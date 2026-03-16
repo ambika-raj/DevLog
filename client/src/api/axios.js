55a92b099c04de8ef1c6fee7c568ac15e38809e5
Ambika Raj <ambikaraj2114@gmail.com>
2026-03-16 12:12:36 +0530

    fix vercel routing

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,20 +1,3 @@
-// import axios from 'axios'  
-// import API from '../api/axios'
-
-// const API = axios.create({
-//   baseURL: 'https://devlog-eis1.onrender.com',
-// })
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token')
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-//   return config
-// })
-
-// export default API
-
 import axios from 'axios'
 
 const API = axios.create({
